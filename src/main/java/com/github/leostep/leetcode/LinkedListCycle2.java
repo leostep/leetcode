0168a5f37e32c33c17a5116374caaf0ebb215364
Leonid Stepanenko <lstepanenko@twilio.com>
2022-07-08 18:42:07 +0200

    142. Linked list cycle 2

---
+++ src/main/java/com/github/leostep/leetcode/LinkedListCycle2.java
@@ -0,0 +1,26 @@
+package com.github.leostep.leetcode;
+
+public class LinkedListCycle2 {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
